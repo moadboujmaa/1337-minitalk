560a940a0ddc49865c040949789a7610821a4776
moadboujamaa <moadboujmaa@gmail.com>
2024-04-01 14:38:09 +0000

    start project

--- client.c
+++ client.c
@@ -6,14 +6,21 @@
 /*   By: mboujama <mboujama@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 10:16:38 by mboujama          #+#    #+#             */
-/*   Updated: 2024/04/01 10:43:36 by mboujama         ###   ########.fr       */
+/*   Updated: 2024/04/01 13:16:35 by mboujama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	printf("Hello, World from client!\n");
+	if (argc == 3)
+	{
+		int server_id = ft_atoi(argv[1]);
+		kill(server_id, SIGUSR1);
+		kill(server_id, SIGUSR2);
+	}
+	else
+		ft_putstr_fd("Executed like this: ./client <pid> <text>", 2);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,43 +6,30 @@
 /*   By: mboujama <mboujama@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 10:16:57 by mboujama          #+#    #+#             */
-/*   Updated: 2024/04/01 12:03:21 by mboujama         ###   ########.fr       */
+/*   Updated: 2024/04/01 13:21:32 by mboujama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void handler_sig()
+void sighandler(int sig)
 {
-	printf("hint\n");
+	if (sig == SIGUSR1)
+		printf("Hello!\n");
+	else if (sig == SIGUSR2)
+		printf("World\n");
 }
 
 int	main(void)
 {
-	int id = fork();
-	
-	if (id == -1)
-		return (1);
+	int pid = getpid();
+	struct sigaction sa;
 
-	if (id == 0)
-	{
-		sleep(2);
-		kill(getppid(), SIGUSR1);
-	}
-	else
-	{
-		struct sigaction sa;
-		sa.sa_flags = SA_RESTART;
-		sa.sa_handler = &handler_sig;
-		sigaction(SIGUSR1, &sa, NULL);
-
-		int x;
-		printf("what is 5 * 3: ");
-		scanf("%d", &x);
-		if (x == 15)
-			printf("Right!");
-		else
-			printf("nope!");
-	}
+	sa.sa_flags = SA_RESTART;
+	sa.sa_handler = &sighandler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	ft_printf("%d\n", pid);
+	while (1);
 	return (0);
 }
